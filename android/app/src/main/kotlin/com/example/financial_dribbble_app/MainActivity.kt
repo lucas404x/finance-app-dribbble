c1642f5a0521678e9eae69878de4c60b597804d5
José Lucas da Silva <joselucas0303@gmail.com>
2022-02-13 14:15:35 -0300

    feat: first commit

---
+++ android/app/src/main/kotlin/com/example/financial_dribbble_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.financial_dribbble_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'app/constants/app_colors.dart';
+import 'app/pages/home/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+      theme: ThemeData(
+          textTheme: GoogleFonts.openSansTextTheme(),
+          backgroundColor: AppColors.backgroundColor,
+          scaffoldBackgroundColor: AppColors.backgroundColor),
+    );
+  }
+}
