d17b8d75a5b018067e84c3d9570791cff7b83c70
José Lucas da Silva <joselucas0303@gmail.com>
2022-02-15 21:07:36 -0300

    feat: add more memojis

--- lib/app/constants/app_assets.dart
+++ lib/app/constants/app_assets.dart
@@ -1,4 +1,11 @@
 class AppAssets {
-  static const String memojiProfile = 'assets/memojis/1.png';
   static const String addCreditCardIcon = 'assets/icons/add_credit_card.png';
+  static const String memojiProfile = 'assets/memojis/1.png';
+  static const List<String> memojisFriends = [
+    'assets/memojis/2.png',
+    'assets/memojis/3.png',
+    'assets/memojis/4.png',
+    'assets/memojis/5.png',
+    'assets/memojis/6.png'
+  ];
 }
