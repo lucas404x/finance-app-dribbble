001b3adbdc5c739de3b5ea031e6c5015b893bb2c
José Lucas da Silva <joselucas0303@gmail.com>
2022-02-20 19:47:49 -0300

    feat: remove scroll glow

--- lib/app/pages/home/home_page.dart
+++ lib/app/pages/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../../core/helpers/scroll_behavior_without_glow.dart';
 import '../../core/models/activity_model.dart';
 import '../../core/models/profile_model.dart';
 import '../../core/repositories/activity_repository.dart';
@@ -20,26 +21,29 @@ class HomePage extends StatelessWidget {
     final List<ActivityModel> _activities = ActivityRepository().getAll();
 
     return SafeArea(
-      child: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 32),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              const HomeHeaderWithProfile(),
-              const Padding(
-                  padding: EdgeInsets.only(top: 24), child: HomeBodyCards()),
-              const Padding(
-                  padding: EdgeInsets.only(top: 24),
-                  child: HomeTransferSection()),
-              Padding(
-                  padding: const EdgeInsets.only(top: 24),
-                  child: ProfileList(profiles: _profiles)),
-              const Padding(
-                  padding: EdgeInsets.only(top: 24),
-                  child: HomeRecentActivitySection()),
-              HomeActivityList(activities: _activities)
-            ],
+      child: ScrollConfiguration(
+        behavior: ScrollBehaviorWithoutGlow(),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 32),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const HomeHeaderWithProfile(),
+                const Padding(
+                    padding: EdgeInsets.only(top: 24), child: HomeBodyCards()),
+                const Padding(
+                    padding: EdgeInsets.only(top: 24),
+                    child: HomeTransferSection()),
+                Padding(
+                    padding: const EdgeInsets.only(top: 24),
+                    child: ProfileList(profiles: _profiles)),
+                const Padding(
+                    padding: EdgeInsets.only(top: 24),
+                    child: HomeRecentActivitySection()),
+                HomeActivityList(activities: _activities),
+              ],
+            ),
           ),
         ),
       ),
